9c382679dc6a3abf1ad432c1621f2a087ab7df1f
John Crispin <john@phrozen.org>
2021-06-16 13:53:00 +0200

    dt-bindings: add en7523-scu device tree binding documentation
    Adds device tree binding documentation for clocks in the
    EN7523 SOC.
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    Signed-off-by: John Crispin <john@phrozen.org>

---
+++ include/dt-bindings/clock/en7523-clk.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#define EN7523_CLK_GSW		0
+#define EN7523_CLK_EMI		1
+#define EN7523_CLK_BUS		2
+#define EN7523_CLK_SLIC		3
+#define EN7523_CLK_SPI		4
+#define EN7523_CLK_NPU		5
+#define EN7523_CLK_CRYPTO	6
+#define EN7523_CLK_PCIE		7
+
+#define EN7523_NUM_CLOCKS	8
